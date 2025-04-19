4515a089f0c1fbb63ef2a57b3e76ca0f7882bc4c
Platon <yomama>
2025-04-19 09:58:41 +0300

    Now it works 14-15

---
+++ src/main/java/org/Reynard_de_Bosnie/lab2tasque5/lab2tasque5_2/Main.java
@@ -0,0 +1,13 @@
+package org.Reynard_de_Bosnie.lab2tasque5.lab2tasque5_2;
+
+import org.Reynard_de_Bosnie.TaskDescription;
+import org.Reynard_de_Bosnie.utils.Solution;
+
+@TaskDescription(taskNumber = 15,
+        taskDescription = "Proxy pattern (to be  solved)")
+public class Main implements Solution {
+    @Override
+    public void execute() throws InterruptedException {
+
+    }
+}
